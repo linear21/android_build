23cacc06381d0e2953c359e5cc09cb05258f3b66
linear21 <ngabriogans7@gmail.com>
2023-07-12 19:19:40 +0700

    Correct detection zip

--- build.sh
+++ build.sh
@@ -67,12 +67,12 @@ function final_build() {
     $MAKE_TARGET
 
     # Upload finished build.
-    BUILD_FILENAME="$(ls out/target/product/$(echo $LUNCH_TARGET | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/recovery.img $(ls out/target/product/$(grep unch main.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip))"
+    BUILD_FILENAME="$(ls out/target/product/$(echo $LUNCH_TARGET | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip)"
     rclone copy $BUILD_FILENAME $RCLONE_NAME:$RCLONE_FOLDER -P
 
     # Upload recovery image build.
-    cp out/target/product/$(echo $LUNCH_TARGET | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/recovery.img $(basename $BUILD_FILENAME .zip).img
-    rclone copy $(basename $BUILD_FILENAME .zip).img $RCLONE_NAME:$RCLONE_FOLDER -P
+    RECOVERY_FILENAME="$(ls out/target/product/$(echo $LUNCH_TARGET | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/recovery.img)"
+    rclone copy $RECOVERY_FILENAME $RCLONE_NAME:$RCLONE_FOLDER -P
 }
 
 # Prepare build environment.
